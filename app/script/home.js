4e88706b89c9fbb5266cafcefa99c7d4cab9d6cb
biplabmalakar <biplab.x@venturepact.com>
2017-10-18 12:32:54 +0530

    part-of-spech graph, repetation list done

--- app/script/home.js
+++ app/script/home.js
@@ -4,14 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const { URL } = require('url');
 const { ipcRenderer } = require('electron');
+const Wordpos = require('wordpos');
 
+let wordpos = new Wordpos();
 let resultObject = {
     noOfChar: 0,
-    nochardot: 0,
     noOfCharSpace: 0,
     noOfWords: 0,
     noOfUnique: 0,
-    repetationOfwORD: 0
+    repetationOfwORD: [],
+    noun: [],
+    verb: [],
+    adverb: [],
+    adjective: []
 };
 let resultMode = 'file';
 
@@ -27,7 +32,6 @@ $('#tableFormat').on('click', () => {
 });
 
 $('#graphFormat').on('click', () => {
-    alert('click');
     if (!($('#graphData').is(':visible'))) {
         $('#tableFormat').removeClass('setDiv');
         $('#tableFormat').addClass('notActive');
@@ -51,36 +55,45 @@ function processFile(data) {
         resultObject.noOfChar = nelineData.length;// number of char with [space, coma, dot]
         setProgress(Math.round((2 / 7) * 100));
         let dotData = nelineData.replace(/[\.,]+/g, ' ');// remove [, and .];
-        let clearData = dotData.split(' ');// total string divided by space
-        clearData = clearData.filter(ele => ele.trim() != ''); //[remove spaces present into array]
-        let originalData = clearData;
-        resultObject.noOfCharSpace = clearData.join('').length; // number of character without[space, dot, comma]
-        setProgress(Math.round((3 / 7) * 100));
+        partOfSpech(dotData)
+            .then(result => {
+                setProgress(Math.round((3 / 7) * 100));
+                resultObject.noun = result.noun;
+                result.verb = result.verb;
+                resultObject.adverb = result.adverb;
+                resultObject.adjective = result.adjective; // find noun, verb, adverb, adjective
 
-        resultObject.noOfWords = originalData.length;
-        setProgress(Math.round((4 / 7) * 100));
+                let clearData = dotData.split(' ');// total string divided by space
+                clearData = clearData.filter(ele => ele.trim() != ''); //[remove spaces present into array]
+                let originalData = clearData;
+                resultObject.noOfCharSpace = clearData.join('').length; // number of character without[space, dot, comma]
+                setProgress(Math.round((4 / 7) * 100));
+                resultObject.noOfWords = originalData.length;
+                setProgress(Math.round((5 / 7) * 100));
 
-        let uniqueWord = [];
-        originalData.map((d, i) => {
-            //alert('word: ' + d + ' index ' + i);
-            if (uniqueWord.findIndex(f => f.word == d.trim().toLowerCase()) == -1) // Find Unique words.
-                uniqueWord.push({ word: d.trim().toLowerCase(), repeat: 0 });
-        });
-        resultObject.noOfUnique = uniqueWord.length;
-        setProgress(Math.round((5 / 7) * 100));
+                let uniqueWord = [];
+                originalData.map((d, i) => {
+                    if (uniqueWord.findIndex(f => f.word == d.trim().toLowerCase()) == -1) // Find Unique words.
+                        uniqueWord.push({ word: d.trim().toLowerCase(), repeat: 0 });
+                });
+                resultObject.noOfUnique = uniqueWord.length;
+                setProgress(Math.round((6 / 7) * 100));
 
-        uniqueWord.map((d, i) => {
-            let wordRepeat = originalData.filter(g => g.toLowerCase().trim() == d.word);
-            if (wordRepeat)
-                uniqueWord[i].repeat = wordRepeat.length - 1; // repetation of each word
-        });
-        resultObject.repetationOfwORD = uniqueWord;
-        setProgress(Math.round((6 / 7) * 100));
-        setTimeout(() => {
-            showMainDiv(true, false, false);
-        }, 500);
-        //alert('Result: ' + JSON.stringify(resultObject));
-        resultMode = file;
+                uniqueWord.map((d, i) => {
+                    let wordRepeat = originalData.filter(g => g.toLowerCase().trim() == d.word);
+                    if (wordRepeat)
+                        uniqueWord[i].repeat = wordRepeat.length - 1; // repetation of each word
+                });
+                resultObject.repetationOfwORD = uniqueWord;
+                setProgress(Math.round((7 / 7) * 100));
+                setTimeout(() => {
+                    showMainDiv(true, false, false);
+                }, 500);
+                resultMode = 'file';
+            })
+            .catch(err => {
+                alert('Error occure: ' + err);
+            });
     }
     else
         alert('No text');
@@ -91,7 +104,11 @@ ipcRenderer.on('file', (event, data) => {
     processFile(data);
 })
 
-
+function partOfSpech(data) {
+    return new Promise((resolve, reject) => {
+        wordpos.getPOS(data, (val) => resolve({ noun: val.nouns, verb: val.verbs, adverb: val.adverbs, adjective: val.adjectives }))
+    });
+}
 
 function setProgress(width) {
     $('#myBar').width(width + '%');
@@ -124,7 +141,7 @@ function initMainDiv() {
         $('.mainDiv .fileList ul').empty();
         $('.mainDiv .fileList ul').hide();
         $('.mainDiv .fileList .emptyText').show();
-        loadData();
+        loadData(resultObject);
     }
     else if (resultMode == 'folder') {
         $('.mainDiv .fileList ul').show();
@@ -141,40 +158,110 @@ function defaultTab() {
     $('#graphData').hide();
     $('#tableData').show();
 }
-function loadData() {
-    defaultTab();
-    $('#totalChar').text(resultObject.noOfChar);
-    $('#totalCharSpace').text(resultObject.noOfCharSpace);
-    $('#totalWord').text(resultObject.noOfWords);
-    $('#totalUniqueWord').text(resultObject.noOfUnique);
-    let chartDiv = $('#chartData');
-    let labell = resultObject.repetationOfwORD.map(d => d.word);
+
+function generateValue(result) {
+    let values = [];
+    values.push(result.noun.length > 0 ? Math.round((result.noun.length / result.noOfWords) * 100) : 0);
+    values.push(result.verb.length > 0 ? Math.round((result.verb.length / result.noOfWords) * 100) : 0);
+    values.push(result.adverb.length > 0 ? Math.round((result.adverb.length / result.noOfWords) * 100) : 0);
+    values.push(result.adjective.length > 0 ? Math.round((result.adjective.length / result.noOfWords) * 100) : 0);
+    return values;
+}
+
+function loadPartOfSpechDiv(result) {
+    if (result.noun.length > 0) {
+        $('.noun').show();
+        $('.noun div').empty();
+        $('.noun h2').text(result.noun.length + ' Noun Present');
+        let str = result.noun.join(', ');
+        $('.noun div').append('<p>' + str + '</p>');
+    }
+    else
+        $('.noun').hide();
+
+    if (result.verb.length > 0) {
+        $('.verb').show();
+        $('.verb div').empty();
+        $('.verb h2').text(result.verb.length + ' Verb Present');
+        let str = result.verb.join(', ');
+        $('.verb div').append('<p>' + str + '</p>');
+    }
+    else
+        $('.verb').hide();
+
+    if (result.adverb.length > 0) {
+        $('.adverb').show();
+        $('.adverb div').empty();
+        $('.adverb h2').text(result.adverb.length + ' Adverb Present');
+        let str = result.adverb.join(', ');
+        $('.adverb div').append('<p>' + str + '</p>');
+    }
+    else
+        $('.adverb').hide();
+
+    if (result.adjective.length > 0) {
+        $('.adjective').show();
+        $('.adjective div').empty();
+        $('.adjective h2').text(result.adjective.length + ' Adjective Present');
+        let str = result.adjective.join(', ');
+        $('.adjective div').append('<p>' + str + '</p>');
+    }
+    else
+        $('.adjective').hide();
+}
+
+function generateColor() {
     let colorArray = [];
-    let values = resultObject.repetationOfwORD.map(d => {
+    let i = 0;
+    while (i < 4) {
         colorArray.push('rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')');
-        return d.repeat != 0 ? Math.round((d.repeat / resultObject.noOfWords) * 100) : 0;
-    });
+        i++;
+    }
+    return colorArray;
+}
+function loadData(result) {
+    defaultTab();
+    $('#totalChar').text(result.noOfChar);
+    $('#totalCharSpace').text(result.noOfCharSpace);
+    $('#totalWord').text(result.noOfWords);
+    $('#totalUniqueWord').text(result.noOfUnique);
+    let colorArray = [];
+    let values = [];
+    colorArray = generateColor();
+    values = generateValue(result);
+    loadPartOfSpechDiv(result);
+    repetationWordSet(result);
+    initChart(colorArray, values);
+}
+
+function repetationWordSet(result) {
+    $('#repetation tbody').empty();
+    for (let word of result.repetationOfwORD)
+        $('#repetation tbody').append('<tr><td style="width: 80%;">' + word.word + '</td><td>' + word.repeat + '</td></tr>');
+}
+function initChart(colorArray, values) {
+    let chartDiv = $('#chartData');
     let chart = new Chart(chartDiv, {
         type: 'bar',
         data: {
-            labels: labell,
+            labels: ['Noun', 'Verb', 'Adverb', 'Adjective'],
             datasets: [{
-                label: 'Word Repetation Percentage',
+                label: 'Part-Of-Spech Percentage Graph',
                 data: values,
                 backgroundColor: colorArray,
                 lineTension: 0
             }]
         },
-        options:{
+        options: {
             responsive: true,
-            scales:{
-                xAxes:[{
+            scales: {
+                xAxes: [{
                     display: true,
                     scaleLabel: {
                         display: true,
-                        labelString: 'Repeat %',
+                        labelString: 'Part-Of-Spech Analyze',
                     },
-                    ticks:{
+                    ticks: {
                         autoSkip: false
                     }
                 }],
@@ -182,14 +269,14 @@ function loadData() {
                     display: true,
                     ticks: {
                         beginAtZero: true,
-                        stepSize : 10,                        
+                        stepSize: 10,
                         max: 100
                     }
                 }]
             },
-            title:{
+            title: {
                 display: true,
-                text: 'Percentage of each word repetation'
+                text: 'Part-Of-Spech Analyze Graph'
             }
         }
     });
